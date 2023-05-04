e75f0de634b714f15da46bdb09dde5ba03f21242
jyzib <jazibzaidi02@gmail.com>
2023-05-04 18:42:56 +0530

    pizza section added

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -23,22 +23,36 @@ const home = () => {
             <div className="date">
             <i className="bi bi-clock"></i>
             <h3>Mon - Sat(10AM - 8PM)</h3>
-            <p>Available Hours</p>
+            <p className='paragraph' >Available Hours</p>
             </div>
             <div className="location">
             <i className="bi bi-geo-alt-fill"></i>
             <h3>Haryana, Faridabad</h3>
-            <p>Our Location</p>
+            <p className='paragraph' >Our Location</p>
 
             </div>
             <div className="contact-no">
             <i className="bi bi-telephone-outbound-fill"></i>
             <h3>91 8920353238</h3>
-            <p>Call us</p>
+            <p className='paragraph' >Call us</p>
 
             </div>
         </div>
-     
+        <div className="round1">
+      <img className='background1' src="img/left-round.png" alt="" />
+      </div>
+      <div className="pizza-section">
+        <div className="pizza">
+            <img className='pizza-img' src="img/pizza.png" alt="" />
+        </div>
+        <div className="pizza-text">
+            <h2 className='Quality' >Quality</h2>
+            <h1 className='Quality-title' >Exploring the Rich Flavors and Diverse of the World</h1>
+            <p className='Quality=text' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora ratione ipsum autem. Architecto molestiae, ex amet dignissimos iste voluptatum harum quasi animi vel sit blanditiis odit perspiciatis labore magni. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis atque illo id quisquam aspernatur iste necessitatibus totam sunt ratione sapiente, aliquid ab perferendis velit. Numquam esse molestiae labore ut aspernatur.</p>
+            <button className='Quick-Order1' >Discover More</button>
+        </div>
+       
+      </div>
     </div>
   )
 }
